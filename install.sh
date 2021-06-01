83258b2122dc15eb20ab7f031f70b4ac3faea792
oshhost <85195927+oshhost@users.noreply.github.com>
2021-06-01 20:23:57 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,64 @@
+os="$(cat /etc/os-release | grep ^ID= | cut -c 4-)"
+url="https://raw.githubusercontent.com/oshhost/nvim-backup/main/init.vim"
+
+if ! hash nvim 2>/dev/null; then
+    case "$os" in 
+        "ubuntu"* )
+            sudo add-apt-repository ppa:neovim-ppa/unstable -y
+            sudo apt update
+            sudo apt -y install neovim
+        ;;
+        "arch"* )
+            yes | sudo pacman -S neovim
+        ;;
+        *)
+            echo The system is not supported.
+            exit 1
+    esac
+fi
+
+read -p "Do you want to symlink /bin/vi and /bin/vim to /bin/nvim? [y/N] " yn
+case $yn in
+    [Yy]* )
+        sudo ln -s /bin/nvim /bin/vi
+        sudo ln -s /bin/nvim /bin/vim
+        ;;
+    * ) break
+        ;;
+esac
+
+read -p "Do you want install custom init.vim? [y/N]" yn
+case $yn in 
+    [Yy]* )
+        mkdir -p ~/.config/nvim/
+        if hash wget 2>/dev/null; then
+            wget -P ~/.config/nvim/ -O init.vim $url
+        elif hash curl 2>/dev/null; then
+            curl -o ~/.config/nvim/init.vim $url
+            echo 1
+        else
+            read -p "Neither wget nor curl is installed. Do you want to install wget? [y/N]" yn
+            case $yn in
+                [Yy]* )
+                    case "$os" in
+                        "ubuntu"* )
+                            sudo apt -y install wget
+                        ;;
+                        "arch"* )
+                            yes | sudo pacman -S wget
+                        ;;
+                    esac
+                    ;;
+                * ) break
+                    ;;
+            esac
+            if hash wget 2>/dev/null; then
+                wget -P ~/.config/nvim/ -O init.vim $url
+            fi
+        fi
+        ;;
+    * ) break
+        ;;
+esac
+
+echo Done.
