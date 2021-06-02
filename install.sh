86396f88b6322f21d85be12163e4a1e938b21383
oshhost <85195927+oshhost@users.noreply.github.com>
2021-06-02 19:35:12 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ mkdir -p ~/.local/bin
 mkdir -p "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload
 
 if ! hash nvim 2>/dev/null; then
-    if [[ ! -e ~/.local/share/nvim/AppRun ]]; then
+    if [ ! -e ~/.local/share/nvim/AppRun ]; then
         DIR=$PWD
         cd ~/.local/share
         echo Downloading Neovim...
@@ -26,14 +26,14 @@ if ! hash nvim 2>/dev/null; then
         NS=1
     fi
 else
-    if [[ ! -e ~/.local/share/nvim/AppRun ]]; then
+    if [ ! -e ~/.local/share/nvim/AppRun ]; then
         for DEST in vi vim nvim; do
             ln -sf "/usr/bin/nvim" "/home/$USER/.local/bin/$DEST"
         done
     fi
 fi
 
-if [[ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]]; then
+if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]; then
     echo Downloading plug.vim...
     wget -O "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
@@ -43,13 +43,13 @@ if ! hash node 2>/dev/null; then
     wget -O- install-node.now.sh/lts | FORCE=1 PREFIX=$HOME/.local bash
 fi
 
-if [[ ! -e ~/.config/nvim/init.vim ]]; then
+if [ ! -e ~/.config/nvim/init.vim ]; then
     mkdir -p ~/.config/nvim
     echo Downloading init.vim...
     wget -O ~/.config/nvim/init.vim $URL
 fi
 
-if [[ -z "$NS" ]]; then
+if [ -z "$NS" ]; then
     nvim +'PlugInstall --sync|source $MYVIMRC'
 else
     bash -c "nvim +'PlugInstall --sync|source $MYVIMRC'"
