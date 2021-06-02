801e3e1d20e08dc3575b7de72334fe4495589b76
oshhost <85195927+oshhost@users.noreply.github.com>
2021-06-02 15:32:20 +0000

    Add files via upload

--- init.vim
+++ init.vim
@@ -1,10 +1,3 @@
-" Neovim only
-let data_dir = stdpath('data').'/site'
-if empty(glob(data_dir.'/autoload/plug.vim'))
-  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
-endif
-
 call plug#begin('~/.config/nvim/plugged')
 
 Plug 'mhinz/vim-startify'
--- install.sh
+++ install.sh
@@ -1,64 +1,20 @@
-os="$(cat /etc/os-release | grep ^ID= | cut -c 4-)"
 url="https://raw.githubusercontent.com/oshhost/nvim-backup/main/init.vim"
 
+mkdir -p ~/.config/nvim/
+
 if ! hash nvim 2>/dev/null; then
-    case "$os" in 
-        "ubuntu"* )
-            sudo add-apt-repository ppa:neovim-ppa/unstable -y
-            sudo apt update
-            sudo apt -y install neovim
-        ;;
-        "arch"* )
-            yes | sudo pacman -S neovim
-        ;;
-        * )
-            echo The system is not supported.
-            exit 1
-    esac
+    dir=$PWD
+    cd ~/.config/nvim/
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    sudo mv nvim.appimage /usr/bin/nvim
+    cd $dir
 fi
 
-read -p "Do you want to symlink /bin/vi and /bin/vim to /bin/nvim? [y/N] " yn
-case $yn in
-    [Yy]* )
-        sudo ln -sf /usr/bin/nvim /usr/bin/vi
-        sudo ln -sf /usr/bin/nvim /usr/bin/vim
-        ;;
-    * );;
-esac
+sudo ln -sf /usr/bin/nvim /usr/bin/vi
+sudo ln -sf /usr/bin/nvim /usr/bin/vim
 
-read -p "Do you want install custom init.vim? [y/N] " yn
-case $yn in 
-    [Yy]* )
-        rm -f ~/.local/share/nvim/site/autoload/plug.vim
-	mkdir -p ~/.config/nvim/
-        if hash wget 2>/dev/null; then
-            wget -O ~/.config/nvim/init.vim $url
-        elif hash curl 2>/dev/null; then
-            curl -o ~/.config/nvim/init.vim $url
-            echo 1
-        else
-            read -p "Neither wget nor curl is installed. Do you want to install wget? [y/N] " yn
-            case $yn in
-                [Yy]* )
-                    case "$os" in
-                        "ubuntu"* )
-                            sudo apt -y install wget
-                        ;;
-                        "arch"* )
-                            yes | sudo pacman -S wget
-                        ;;
-                    esac
-                    ;;
-                * );;
-            esac
-            if hash wget 2>/dev/null; then
-                wget -P ~/.config/nvim/ -O init.vim $url
-            fi
-        fi
-        ;;
-    * );;
-esac
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -o ~/.config/nvim/init.vim $url
 
-killall yes
-
-nvim
+nvim +PlugInstall
