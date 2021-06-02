53037736ca265c655ab53a4a6867965538f3e3d6
oshhost <85195927+oshhost@users.noreply.github.com>
2021-06-02 18:56:14 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -36,7 +36,7 @@ fi
 
 if [[ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]]; then
     echo Downloading plug.vim...
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 if ! hash node 2>/dev/null; then
