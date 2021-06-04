f77175d1b32fc51e4fbdf2d7ce6b39b644ba1d0a
oshhost <85195927+oshhost@users.noreply.github.com>
2021-06-04 12:26:47 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ if ! hash nvim 2>/dev/null; then
         DIR=$PWD
         cd /tmp
         echo Downloading Neovim...
-        wget -q https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+        wget --show-progress -q https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
         chmod u+x nvim.appimage
-        ./nvim.appimage --appimage-extract
+        ./nvim.appimage --appimage-extract >/dev/null
         rm nvim.appimage
         shopt -s dotglob
         mv squashfs-root/* ~/.local/share/nvim
@@ -46,14 +46,18 @@ if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]; then
 fi
 
 if ! hash git 2>/dev/null; then
-    DIR=$PWD
-    cd ~/.config/nvim/plugged
-    echo Git not found. Downloading plugins via wget... 
-    cat ~/.config/nvim/init.vim | grep "Plug '" | sed -e "s/^.*Plug [']//" -e "s/'.*//" | grep -v git | while read REP; if [ "$REP" = "" ]; then break; fi; OUT=$(echo $REP | sed -e "s/.*\///").tgz; do wget --show-progress -qO $OUT github.com/$REP/tarball/master; done
-    ls | grep .tgz$ | while read TAR; do tar xf $TAR; done
-    rm *.tgz
-    ls | while read REP; do mv $REP $(echo $REP | sed -e "s/^[^-]*-//" -e "s/\(.*\)-.*/\1/"); done
-    cd $DIR
+    if [ -z "$(ls -A ~/.config/nvim/plugged)" ]; then
+        DIR=$PWD
+        cd ~/.config/nvim/plugged
+        echo Git not found. Downloading plugins via wget... 
+        cat ~/.config/nvim/init.vim | grep "Plug '" | sed -e "s/^.*Plug [']//" -e "s/'.*//" | grep -v git | while read REP; if [ "$REP" = "" ]; then break; fi; OUT=$(echo $REP | sed -e "s/.*\///").tgz; do wget --show-progress -qO $OUT github.com/$REP/tarball/master; done
+        ls | grep .tgz$ | while read TAR; do tar xf $TAR; done
+        rm *.tgz
+        ls | while read REP; do mv $REP $(echo $REP | sed -e "s/^[^-]*-//" -e "s/\(.*\)-.*/\1/"); done
+        cd $DIR
+    else
+        echo ~/.config/nvim/plugged already contain some files. Assuming the plugins are installed...
+    fi
 fi
 
 if ! hash node 2>/dev/null; then
