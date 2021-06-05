ac0133718e2ed8c8d5ea1656f5d074204f775f90
oshhost <85195927+oshhost@users.noreply.github.com>
2021-06-05 19:37:27 +0300

    Add files via upload

---
+++ uninstall-nvim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+read -p "Do you really want to uninstall Neovim? [y/N] " yn
+case $yn in
+    [Yy]* )
+        read -p "All configuration files will be deleted. Continue? [y/N] " ync
+        case $ync in 
+            [Yy]* )
+                echo Removing symlinks in ~/.local/bin...
+                rm ~/.local/bin/vi ~/.local/bin/vim ~/.local/bin/nvim
+                echo Removing data and config files...
+                rm -rf ~/.local/share/nvim ~/.config/nvim ~/.config/coc
+        esac
+        ;;
+esac
---
+++ update-nvim.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [[ "$(nvim -v | head -n1 | cut -c6-)" != "$(wget -qO- api.github.com/repos/neovim/neovim/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')" ]]; then
+    DIR=$PWD
+    cd /tmp
+    echo Downloading Neovim...
+    wget --show-progress -q github.com/neovim/neovim/releases/latest/download/nvim.appimage
+    chmod u+x nvim.appimage
+    echo Extracting...
+    ./nvim.appimage --appimage-extract >/dev/null
+    rm nvim.appimage
+    echo Moving content to ~/.local/share/nvim...
+    mv squashfs-root/* ~/.local/share/nvim
+    rm -rf squashfs-root
+    cd $DIR
+else
+    echo Neovim is up to date.
+fi
