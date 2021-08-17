b0b1e197407cded7daaf70bbde5478f12d875e42
oshhost <85195927+oshhost@users.noreply.github.com>
2021-08-17 16:02:36 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -3,92 +3,118 @@ UNINSTALL="raw.githubusercontent.com/oshhost/nvim-backup/main/uninstall-nvim.sh"
 UPDATE="raw.githubusercontent.com/oshhost/nvim-backup/main/update-nvim.sh"
 NODE="install-node.vercel.app/lts"
 
-CB=$(tput setaf 4);CG=$(tput setaf 2);CD=$(tput sgr0)
+CC=$(tput setaf 6);CB=$(tput setaf 4);CG=$(tput setaf 2);CR=$(tput setaf 1);CD=$(tput sgr0)
 echo
-echo "${CB}ooooo      ooo${CG}                                 o8o"
-echo "${CB}\`888b.     \`8'${CG}                                 \`\"'"
-echo "${CB} 8 \`88b.    8   .ooooo.   .ooooo.${CG} oooo    ooo oooo  ooo. .oo.  .oo."
-echo "${CB} 8   \`88b.  8  d88' \`88b d88' \`88b${CG} \`88.  .8'  \`888  \`888P\"Y88bP\"Y88b"
-echo "${CB} 8     \`88b.8  888ooo888 888   888${CG}  \`88..8'    888   888   888   888"
-echo "${CB} 8       \`888  888    .o 888   888${CG}   \`888'     888   888   888   888"
-echo "${CB}o8o        \`8  \`Y8bod8P' \`Y8bod8P'${CG}    \`8'     o888o o888o o888o o888o${CD}"
+echo "${CB}ooooo      ooo${CG}                                 o8o                                  ${CC}  .oooooo."
+echo "${CB}\`888b.     \`8'${CG}                                 \`\"'                                  ${CC} d8P'  \`Y8b"
+echo "${CB} 8 \`88b.    8   .ooooo.   .ooooo.${CG} oooo    ooo oooo  ooo. .oo.  .oo.    ${CR},d88b.d88b,  ${CC}888            .ooooo."
+echo "${CB} 8   \`88b.  8  d88' \`88b d88' \`88b${CG} \`88.  .8'  \`888  \`888P\"Y88bP\"Y88b   ${CR}88888888888  ${CC}888           d88' \`88b"
+echo "${CB} 8     \`88b.8  888ooo888 888   888${CG}  \`88..8'    888   888   888   888   ${CR}\`Y8888888Y'  ${CC}888     ooooo 888   888"
+echo "${CB} 8       \`888  888    .o 888   888${CG}   \`888'     888   888   888   888     ${CR}\`Y888Y'    ${CC}\`88.    .88'  888   888"
+echo "${CB}o8o        \`8  \`Y8bod8P' \`Y8bod8P'${CG}    \`8'     o888o o888o o888o o888o      ${CR}\`Y'      ${CC} \`Y8bood8P'   \`Y8bod8P'"
 echo
 
 mkdir -p ~/.local/bin
 mkdir -p ~/.local/share/nvim/site/autoload
 mkdir -p ~/.config/nvim/plugged
+DIR=$PWD
 
 if ! hash nvim 2>/dev/null; then
-    if [ ! -e ~/.local/share/nvim/AppRun ]; then
-        DIR=$PWD
-        cd ~/.local/bin
-        echo Downloading Neovim...
-        wget --show-progress -q github.com/neovim/neovim/releases/latest/download/nvim.appimage
-        chmod u+x nvim.appimage
-        mv nvim.appimage nvim
-        echo Symlinking vi and vim to nvim \(~/.local/bin\)... You might want to change this behaviour manually.
-        for DEST in vi vim; do
-            ln -sf "$HOME/.local/bin/nvim" "$HOME/.local/bin/$DEST"
-        done
-        echo && echo To uninstall and update neovim, use uninstall-nvim.sh and update-nvim.sh scripts accordingly \(~/.local/bin\)...
-        wget --show-progress -q $UNINSTALL $UPDATE
-        chmod +x uninstall-nvim.sh update-nvim.sh
-        cd $DIR
-    fi
-    if ! hash nvim 2>/dev/null; then
-        echo Prepending ~/.local/bin to PATH \(~/.bashrc\)... You might want to change this behaviour manually.
-        echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
-        PATH=$HOME/.local/bin:$PATH
-    fi
+	if [ ! -e ~/.local/share/nvim/AppRun ]; then
+		cd ~/.local/bin
+		echo Downloading Neovim...
+		wget --show-progress -q github.com/neovim/neovim/releases/latest/download/nvim.appimage
+		chmod u+x nvim.appimage
+		mv nvim.appimage nvim
+		echo Symlinking vi and vim to nvim \(~/.local/bin\)... You might want to change this behaviour manually.
+		for DEST in vi vim; do
+			ln -sf "$HOME/.local/bin/nvim" "$HOME/.local/bin/$DEST"
+		done
+		echo && echo To uninstall and update neovim, use uninstall-nvim.sh and update-nvim.sh scripts accordingly \(~/.local/bin\)...
+		wget --show-progress -q $UNINSTALL $UPDATE
+		chmod +x uninstall-nvim.sh update-nvim.sh
+	fi
+	if ! hash nvim 2>/dev/null; then
+		echo Prepending ~/.local/bin to PATH \(~/.bashrc\)... You might want to change this behaviour manually.
+		echo 'PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
+		PATH=$HOME/.local/bin:$PATH
+	fi
 else
-    if [ ! -e ~/.local/share/nvim/AppRun ]; then
-        echo Symlinking vi and vim to nvim \(~/.local/bin\)... You might want to change this behaviour manually.
-        for DEST in vi vim; do
-            ln -sf "/usr/bin/nvim" "$HOME/.local/bin/$DEST"
-        done
-    fi
+	if [ ! -e ~/.local/share/nvim/AppRun ]; then
+		echo Symlinking vi and vim to nvim \(~/.local/bin\)... You might want to change this behaviour manually.
+		for DEST in vi vim; do
+			ln -sf "/usr/bin/nvim" "$HOME/.local/bin/$DEST"
+		done
+	fi
 fi
 echo
 
-if [ ! -e ~/.config/nvim/init.vim ]; then
-    echo Downloading init.vim...
-    wget --show-progress -qO ~/.config/nvim/init.vim $INIT
-    echo
-fi
+echo Synchronizing init.vim...
+wget --show-progress -qO ~/.config/nvim/init.vim $INIT
+echo
 
 if [ ! -e ~/.local/share/nvim/site/autoload/plug.vim ]; then
-    echo Downloading plug.vim...
-    wget --show-progress -qO ~/.local/share/nvim/site/autoload/plug.vim raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    echo
+	echo Downloading plug.vim...
+	wget --show-progress -qO ~/.local/share/nvim/site/autoload/plug.vim raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+	echo
 fi
 
 if ! hash git 2>/dev/null; then
-    NO_GIT=1
-    if [ -z "$(ls -A ~/.config/nvim/plugged)" ]; then
-        DIR=$PWD
-        cd ~/.config/nvim/plugged
-        echo Git not found. Downloading plugins via wget... 
-        echo Skipping plugins containing the \'git\' substring...
-        cat ~/.config/nvim/init.vim | grep "Plug '" | grep -v git | sed -e "s/^.*Plug [']//" -e "s/'.*//" | while read REP; if [ "$REP" = "" ]; then break; fi; OUT=$(echo $REP | sed -e "s/.*\///").tgz; do if [ "$REP" = "neoclide/coc.nvim" ]; then REF=release; else REF=master; fi; wget --show-progress -qO $OUT github.com/$REP/tarball/$REF; done
-        echo Extracting...
-        ls | grep .tgz$ | while read TAR; do TE=${TAR%.tgz}; mkdir $TE; tar xf $TAR -C $TE; cd $TE; TD=$(ls); mv $TD/* .; mv $TD/.* . 2>/dev/null; rmdir $TD; cd ..; done
-        rm *.tgz
-        cd $DIR
-    else
-        echo ~/.config/nvim/plugged already contains some files. Assuming the plugins are installed...
-    fi
-    echo
+	NO_GIT=1
+	if [ -z "$(ls -A ~/.config/nvim/plugged)" ]; then
+		cd ~/.config/nvim/plugged
+		echo Git not found. Downloading plugins via wget...
+		echo Skipping plugins containing the \'git\' substring...
+		cat ~/.config/nvim/init.vim | grep "Plug '" | grep -v git | sed -e "s/^.*Plug [']//" -e "s/'.*//" | while read REP; if [ "$REP" = "" ]; then break; fi; OUT=$(echo $REP | sed -e "s/.*\///").tgz; do if [ "$REP" = "neoclide/coc.nvim" ]; then REF=release; else REF=master; fi; wget --show-progress -qO $OUT github.com/$REP/tarball/$REF; done
+		echo Extracting...
+		ls | grep .tgz$ | while read TAR; do TE=${TAR%.tgz}; mkdir $TE; tar xf $TAR -C $TE; cd $TE; TD=$(ls); mv $TD/* .; mv $TD/.* . 2>/dev/null; rmdir $TD; cd ..; done
+		rm *.tgz
+	else
+		echo ~/.config/nvim/plugged already contains some files. Assuming the plugins are installed...
+	fi
+	echo
 fi
 
 if ! hash node 2>/dev/null; then
-    echo Installing Node.js...
-    wget --show-progress -qO- $NODE | FORCE=1 PREFIX=~/.local bash >/dev/null 2>&1
-    echo
+	echo Installing Node.js...
+	wget --show-progress -qO- $NODE | FORCE=1 PREFIX=~/.local bash >/dev/null 2>&1
+	echo
+fi
+
+LATEST="$(wget -qO- 'https://golang.org/VERSION?m=text')"
+DL_PKG="$LATEST.linux-amd64.tar.gz"
+DL_URL="https://golang.org/dl/$DL_PKG"
+
+function current_go() {
+	echo $(go version | grep -oP 'go[0-9.]+')
+}
+
+function download_latest_go() {
+	cd /tmp
+	echo "Downloading latest Go for Linux AMD64: ${LATEST}."
+	wget --no-check-certificate --continue --show-progress "$DL_URL" -P "$GOUTIL"
+	rm -rf "$HOME/go" && tar -C $HOME -xzf "$DL_PKG"
+	echo "Updated Go to version $(current_go)."
+}
+
+if hash go 2>/dev/null; then
+	CURRENT="$(current_go)"
+	if ! ["$CURRENT" == "$LATEST" ]; then
+		echo "Updating $CURRENT to $LATEST."
+		download_latest_go
+	else
+		echo "$CURRENT is up to date."
+	fi
+else
+	echo Installing Golang...
+	download_latest_go
 fi
 
 echo The installation is complete.
 sleep 1
 echo
+
+cd $DIR
 
 ARG="let g:startify_custom_header=startify#fortune#cowsay(['Thank you for installing Neovim!'])|sil Startify"
 
